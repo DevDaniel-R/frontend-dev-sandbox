1abf2edb2ba178b3711e98ef2e308d176a40dcf6
Daniel Rodriguez <Drodriguez661@gmail.com>
2023-12-08 16:52:39 -0600

    using array method reduce

--- javascript-notes/arrays/using-reduce.js
+++ javascript-notes/arrays/using-reduce.js
@@ -10,12 +10,5 @@ const menuItems = [
     { item: "Ponzu Glazed Salmon", price: 23 },
     { item: "Philly Cheese Steak", price: 13 },
     { item: "Baked Italian Chicken Sub", price: 12 },
-    { item: "Pan Seared Ribeye", price: 31 },
-    { item: "Blue Cheese Salad", price: 8 },
-    { item: "Spicey Chicken Rigatoni", price: 18 },
-    { item: "Ponzu Glazed Salmon", price: 23 },
-    { item: "Philly Cheese Steak", price: 13 },
-    { item: "Baked Italian Chicken Sub", price: 12 },
     { item: "Pan Seared Ribeye", price: 31 }
-
 ];
