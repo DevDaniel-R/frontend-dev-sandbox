12364352e30b6f1a7f716e4e6f0b690e1da36d86
Daniel Rodriguez <Drodriguez661@gmail.com>
2024-01-04 01:10:12 -0600

    practice with functions

--- javascript-notes/functions/brad/brad-function-basics.js
+++ javascript-notes/functions/brad/brad-function-basics.js
@@ -10,3 +10,19 @@ function add(num1, num2) {
 //When calling function you pass in the number that you want to add 5 & 10 these are arguments
 
 add(5, 10);
+
+//! Immediately Invoked Function Expresions
+(function () {
+    const user = 'John';
+    console.log(user);
+    const hello = () => console.log('Hello from the IIFE')
+    hello();
+})();
+
+(function (name) {
+    console.log('Hello ' + name);
+})('Shawn');
+
+(function hello() {
+    console.log('Hello');
+})();
