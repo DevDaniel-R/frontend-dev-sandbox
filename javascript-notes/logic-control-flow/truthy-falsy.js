6ea337e9eaac9da008854aab68bc81185e4e1be8
Daniel Rodriguez <Drodriguez661@gmail.com>
2024-01-08 00:36:24 -0600

    truthy & falsey

--- javascript-notes/logic-control-flow/truthy-falsy.js
+++ javascript-notes/logic-control-flow/truthy-falsy.js
@@ -1,1 +1,26 @@
-//
+//TODO Falsy Values : FALSE, 0, "" or '' (empty string), NULL, UNDEFINED, NaN
+const x = false;
+//? Checking for empty arrays
+const post = ["Post One"];
+
+if (post.length > 0) {
+    console.log('List posts');
+} else {
+    console.log('No Post to List');
+}
+
+//?Checking for empty objects
+    const user = {
+        name: 'Brad',
+    };
+
+    if (Object.keys(user).length > 0) {
+        console.log('list User');
+    } else {
+        console.log('No User')
+    }
+
+//? Loose Equality ( == ) We should stick to using triple (===) because it checks type aswell.
+    console.log(false === 0);
+    console.log('' === 0);
+    console.log(null === undefined);
