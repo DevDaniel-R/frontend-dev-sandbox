249a6d50af15c97abf1ccca604d58b62213a962f
Daniel Rodriguez <Drodriguez661@gmail.com>
2024-02-02 18:54:13 -0600

    dom practice

--- javascript-notes/DOM/08-list-item-innerHTML-vs-createElement/shopping-list/script2.js
+++ javascript-notes/DOM/08-list-item-innerHTML-vs-createElement/shopping-list/script2.js
@@ -1,21 +1,58 @@
-// insertAdjacentElement EX
+//TODO insertAdjacentElement EX
 function insertElement() {
     const filter = document.querySelector('.filter');
 
     const h1 = document.createElement('h1');
-    h1.insertAdjacentElement()
+    h1.textContent = 'insertAdjacentElement'
+    const p = document.createElement('p');
+    p.innerHTML = 'Hello There'
+
+    filter.insertAdjacentElement('beforebegin', p);
 }
 
-// insertAdjacentText EX
+//TODO insertAdjacentText EX
 
-//insertAdjacentHTML EX
+function insertText() {
+    const item = document.querySelector('li:first-child');
+    item.insertAdjacentText('afterend', 'This is after end')
+}
 
-//insertBefore EX
+//TODO insertAdjacentHTML EX
 
-//* beforebegin
+function insertHTML() {
+    const clearBtn = document.querySelector('#clear');
 
-//* afterbegin
+    clearBtn.insertAdjacentHTML('beforebegin', '<h2>insert Adjacent html</h2>')
+}
 
-//* beforeend
+//TODO insertBefore EX
 
-//* afterend
+
+
+// insertElement();
+// insertText();
+// insertHTML()
+
+function insertAfter(newEl, existingEl) {
+    existingEl.parentElement.insertBefore(newEl, existingEl.nextSibling);
+}
+
+function insertBefore(newEle, existingEle) {
+    existingEle.parentElement.insertBefore(newEle, existingEle.nextSibling);
+}
+
+//New element to insert
+const li = document.createElement('li');
+li.textContent = 'Insert Me After!';
+
+const p = document.createElement('p');
+p.textContent = 'Im p'
+
+//Existing element to insert after
+const firstItem = document.querySelector('li:first-child');
+
+const secondItem = document.querySelector('li:last-child');
+
+// Our custom function 
+insertAfter(li,firstItem);
+insertBefore(p,secondItem);
